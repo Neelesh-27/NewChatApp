4f42ef0ee6ac0e7588cb3245485f2ff2e5b81aa2
Neelesh-27 <neeleshwaghade@gmail.com>
2019-08-28 02:33:55 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { GroupComponent } from './group/group.component';
+import { ChatComponent } from './chat/chat.component';
+import { SelectComponent } from './select/select.component';
+const appRoutes: Routes = [
+  { path: 'user', component: UserComponent },
+  { path: 'group',      component: GroupComponent },
+  { path: 'chat',      component: ChatComponent },
+  { path: 'select',      component: SelectComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    GroupComponent,
+    ChatComponent,
+    SelectComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } 
+    ),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
